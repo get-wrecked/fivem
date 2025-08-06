0f1cb03e757ad4bdc11764c22ba36c752786acbc
Kyle Graham <kylegraham05@gmail.com>
2025-08-06 12:05:33 -0700

    Add basic FiveM resource boilerplate

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+
+shared_script 'shared/config.lua'
+client_scripts { 'client/*.lua' }
+server_scripts { 'server/*.lua' }
