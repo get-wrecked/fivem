48349db1fed60a8c330b2cbfd1341a3870d520f1
Imthatguyhere <imthatguyhere@gmail.com>
2025-08-08 11:09:11 -0400

    build: set up Vite + Typescript + React + Tailwindcss v4

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,21 @@ game 'gta5'
 use_experimental_fxv2_oal 'yes'
 lua54 'yes'
 
-shared_script 'shared/config.lua'
-client_scripts { 'client/*.lua' }
-server_scripts { 'server/*.lua' }
+shared_scripts {
+  'shared/config.lua'
+}
+client_scripts { 
+  'client/*.lua' ,
+}
+server_scripts {
+  'server/*.lua' ,
+}
+
+
+
+files {
+  'ui/dist/index.html',
+  'ui/dist/**/*',
+}
+
+ui_page 'ui/dist/index.html'
