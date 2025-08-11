dbfae873ba6fe51059314d57ac70d25e77b9eefe
Kyle Graham <kylegraham05@gmail.com>
2025-08-11 09:09:28 -0700

    Switch Tailwindcss to PostCSS

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+    plugins: [
+        react()
+    ],
+    base: './',
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src')
+        }
+    }
 })
