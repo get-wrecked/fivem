9192ccb364a4385aaf70635814ad7a802fda3cbc
Imthatguyhere <imthatguyhere@gmail.com>
2025-08-12 15:29:27 -0400

    build(ui): add TSFX, and initialize it in React

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { NuiProvider, NuiVisibilityProvider } from "@tsfx/hooks";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <NuiProvider>
+    <NuiVisibilityProvider>
+      <App />
+    </NuiVisibilityProvider>
+    </NuiProvider>
   </StrictMode>
 )
