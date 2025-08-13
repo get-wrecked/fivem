a5b6c4eaa45b71bf3eab66569e6cc589a8cc8898
Kyle Graham <kylegraham05@gmail.com>
2025-08-13 08:30:48 -0700

    Create NUI Container component

---
+++ ui/src/components/container.tsx
@@ -0,0 +1,46 @@
+import type React from 'react';
+import type { PropsWithChildren } from 'react';
+import logo from '../assets/logo.svg';
+
+const Header: React.FC = () => {
+    return (
+        <div className='w-full h-14 bg-second-layer flex items-center justify-between'>
+            <span className='grow px-6 py-4'>
+                <img src={logo} alt='Medal Logo' className='h-6' />
+            </span>
+
+            <button
+                type='button'
+                className='size-14 flex items-center justify-center text-foreground-300 hover:bg-accent-secondary hover:text-foreground-0'
+            >
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    role='img'
+                    aria-label='Close'
+                >
+                    <path d='M18 6 6 18' />
+                    <path d='m6 6 12 12' />
+                </svg>
+                <span className='sr-only'>Close</span>
+            </button>
+        </div>
+    );
+};
+
+export const Container: React.FC<PropsWithChildren> = ({ children }) => {
+    return (
+        <div className='absolute right-0 top-1/2 mr-8 -translate-y-1/2 w-[360px] h-[420px] bg-third-layer rounded-lg overflow-hidden flex flex-col'>
+            <Header />
+
+            <div className='w-full grow py-4 px-6'>{children}</div>
+        </div>
+    );
+};
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,10 +2,11 @@ import type React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { Container } from './components/container';
 import Providers from './providers';
 
-const App: React.FC = () => {
-    return <span>TODO</span>;
+export const App: React.FC = () => {
+    return <Container>TODO</Container>;
 };
 
 const rootElement = document.getElementById('root');
