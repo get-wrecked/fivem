3b0ce171c812104f396920332291fbbb5fe09d18
Kyle Graham <kylegraham05@gmail.com>
2025-08-13 09:39:49 -0700

    Setup NUI Auto Clipping Scroll Area container

--- ui/src/components/auto-clipping.tsx
+++ ui/src/components/auto-clipping.tsx
@@ -1,5 +1,10 @@
 import type React from 'react';
+import { ScrollArea } from './ui/scroll-area';
 
 export const AutoClipping: React.FC = () => {
-    return <div className='w-full grow'></div>;
+    return (
+        <div className='w-full grow' style={{ containerType: 'size' }}>
+            <ScrollArea style={{ height: '100cqh' }}></ScrollArea>
+        </div>
+    );
 };
