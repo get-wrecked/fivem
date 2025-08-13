c54817e0c6cb2b661a9b8a0cbab877b25f9487ef
Kyle Graham <kylegraham05@gmail.com>
2025-08-13 09:10:56 -0700

    Finalize NUI App layout

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,6 +2,8 @@ import type React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { AutoClipping } from './components/auto-clipping';
+import { ClipLength } from './components/clip-length';
 import { Container } from './components/container';
 import { ServerDetails } from './components/server-details';
 import Providers from './providers';
@@ -10,6 +12,8 @@ export const App: React.FC = () => {
     return (
         <Container>
             <ServerDetails />
+            <AutoClipping />
+            <ClipLength />
         </Container>
     );
 };
