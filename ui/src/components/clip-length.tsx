25d8010dac7cf5251d1c83852d2f6fa7a1b99b81
Kyle Graham <kylegraham05@gmail.com>
2025-08-15 10:15:50 -0700

    Create Clip Length selection component

--- ui/src/components/clip-length.tsx
+++ ui/src/components/clip-length.tsx
@@ -1,5 +1,27 @@
 import type React from 'react';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import { useState } from 'react';
 
 export const ClipLength: React.FC = () => {
-    return <div className='w-full h-9'></div>;
+    const [length, setLength] = useState<string>('30');
+
+    return (
+        <div className='w-full h-9 flex items-center justify-between'>
+            <span>Clip Length</span>
+
+            <Select value={length} onValueChange={setLength}>
+                <SelectTrigger className='w-32'>
+                    <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value='15'>15 seconds</SelectItem>
+                    <SelectItem value='30'>30 seconds</SelectItem>
+                    <SelectItem value='1'>1 minute</SelectItem>
+                    <SelectItem value='2'>2 minutes</SelectItem>
+                    <SelectItem value='3'>3 minutes</SelectItem>
+                    <SelectItem value='5'>5 minutes</SelectItem>
+                </SelectContent>
+            </Select>
+        </div>
+    );
 };
