3ba3ab8c04f86a5defe4e24cd418755a5a6fd92a
Kyle Graham <kylegraham05@gmail.com>
2025-08-19 07:24:12 -0700

    chore: center NUI server details title

--- ui/src/components/server-details.tsx
+++ ui/src/components/server-details.tsx
@@ -1,12 +1,8 @@
 export const ServerDetails: React.FC = () => {
     return (
-        <div className='w-full h-12 flex gap-2'>
+        <div className='w-full h-12 flex gap-2 items-center'>
             <img src='https://picsum.photos/64' alt='Server Logo' className='size-12 rounded-lg' />
-            <h4 className='line-clamp-2 font-medium'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, alias porro. Cum
-                dicta totam sed nulla soluta, fugiat quam? Dignissimos obcaecati corporis qui animi
-                doloribus, nulla exercitationem ab est repudiandae.
-            </h4>
+            <h4 className='line-clamp-2 font-medium'>Test Server</h4>
         </div>
     );
 };
