e48411c15157133dec1e88e504ad23ee7ae0d5e4
Kyle Graham <kylegraham05@gmail.com>
2025-08-20 07:17:57 -0700

    feat: create shared resource logger

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,20 +5,21 @@ lua54 'yes'
 
 shared_scripts {
   'config.lua',
+  'lib/shared-logger.lua',
 }
-client_scripts { 
+
+client_scripts {
   'clipping/client-main.lua',
   'datavein/client-main.lua',
   'screenshot/client-main.lua',
   'lib/client-main.lua',
 }
+
 server_scripts {
   'clipping/server-main.lua' ,
   'datavein/server-main.lua',
   'lib/server-main.lua',
 }
-
-
 
 files {
   'ui/dist/index.html',
---
+++ lib/shared-logger.lua
@@ -0,0 +1,43 @@
+local levels = {
+    error = 1,
+    warning = 2,
+    info = 3,
+    debug = 4
+}
+
+local prefixes = {
+    '^1[ERROR]',
+    '^3[WARN]',
+    '^7[INFO]',
+    '^6[DEBUG]'
+}
+
+local function log(level, ...)
+    if level == levels.debug and not Config.Debug then
+        return
+    end
+
+    local args = { ... }
+
+    local jsonException = function (reason, value)
+        if type(value) == 'function' then
+            return tostring(value)
+        end
+
+        return reason
+    end
+
+    for i = 1, #args do
+        local arg = args[i]
+        args[i] = type(arg) == 'table' and json.encode(arg, { indent = true, sort_keys = true, exception = jsonException }) or tostring(arg)
+    end
+
+    print(('^5[Medal] %s %s^7'):format(prefixes[level], table.concat(args, '\t')))
+end
+
+Logger = {
+    error = function (...) log(levels.error, ...) end,
+    warning = function (...) log(levels.warning, ...) end,
+    info = function (...) log(levels.info, ...) end,
+    debug = function (...) log(levels.debug, ...) end,
+}
