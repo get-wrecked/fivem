666fb9c605f4ca10adde8def841fc01c2bc6390a
Kyle Graham <kylegraham05@gmail.com>
2025-08-20 09:52:18 -0700

    chore(NUI): remove duplicate UI visibility events

--- ui/src/handlers/nui-handlers.tsx
+++ ui/src/handlers/nui-handlers.tsx
@@ -1,24 +1,8 @@
-import React from 'react';
-import { useNuiEvent, useNuiVisibility } from '@tsfx/hooks';
+import type React from 'react';
 
 //=-- Centralized NUI message listeners for messages coming from Lua/CFX
 //=-- This component registers event handlers via @tsfx/hooks and renders nothing
 export const NuiHandlers: React.FC = () => {
-    const { setVisible } = useNuiVisibility();
-
-    //=-- Visibility controls (common pattern)
-    useNuiEvent<boolean>('ui:setVisible', {
-        handler: (v) => setVisible(Boolean(v)),
-    });
-
-    useNuiEvent<void>('ui:open', {
-        handler: () => setVisible(true),
-    });
-
-    useNuiEvent<void>('ui:close', {
-        handler: () => setVisible(false),
-    });
-
     //=-- Ex: Add more handlers below as your server/client scripts emit events
     //=-- useNuiEvent<TypeGoesHere>('your:event:name', { handler: (payload) => { /*//=-- do thing */ } });
 
