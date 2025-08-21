1131766da4a9387ae2ffe4aa14a60a8473ceceb9
Imthatguyhere <imthatguyhere@gmail.com>
2025-08-21 17:32:58 -0400

    feat(gameVein): set up player name gathering
    with a FiveM default for no framework

---
+++ gameVein/ore/client-name.lua
@@ -0,0 +1,30 @@
+--//=-- GameVein Ore: Player Name
+--//=-- Retrieves the player's name, differing depending on the server's framework
+
+GameVein = GameVein or {}
+GameVein.Ore = GameVein.Ore or {}
+
+local framework = 'none'
+
+--- Get the current client's player name
+--- @return string name The player's name or "unknown"
+local function getFivemName()
+    local name = GetPlayerName(-1) --//=-- The current player's name
+    if type(name) == 'string' and #name > 0 then
+        return name
+    else
+        return 'unknown'
+    end
+end
+
+--- Get the current client's player name
+--- @return string name The player's name or "unknown"
+function GameVein.Ore.name()
+  local playerName = 'unknown'
+
+  if not framework or framework == 'none' then
+    playerName = getFivemName()
+  end
+
+  return playerName
+end
