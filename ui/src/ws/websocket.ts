b7de583feb96b5519923d11ee0454dbb6a9ac335
Imthatguyhere <imthatguyhere@gmail.com>
2025-08-21 10:54:30 -0400

    feat(ws): import types

---
+++ ui/src/ws/websocket.ts
@@ -0,0 +1,3 @@
+//=-- WebSocket client public module: types + implementation
+import { WsConfig, WsEvent, MessageHandler, OpenHandler, ErrorHandler, CloseHandler } from './types';
+export * from './types';
