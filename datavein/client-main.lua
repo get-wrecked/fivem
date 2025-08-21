8f2512331b4d197fbe83205c2356095fb6d63191
Imthatguyhere <imthatguyhere@gmail.com>
2025-08-21 13:02:13 -0400

    feat(dataVein): add type for config

--- datavein/client-main.lua
+++ datavein/client-main.lua
@@ -1,1 +1,6 @@
---==// Placeholder
+---@class WsConfigLua
+---@field host? string          # WebSocket host (default 127.0.0.1)
+---@field port? integer         # WebSocket port (default 63325)
+---@field protocol? 'ws'|'wss'  # WebSocket scheme (default 'ws')
+---@field path? string          # Optional path, ex: `/socket`
+
