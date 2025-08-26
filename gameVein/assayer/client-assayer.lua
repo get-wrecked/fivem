dde2e3984e515acaa7e8bc66a8dfb7f149f6a250
Imthatguyhere <imthatguyhere@gmail.com>
2025-08-26 14:37:01 -0400

    feat(ore): scaffolding for the ore job

--- gameVein/assayer/client-assayer.lua
+++ gameVein/assayer/client-assayer.lua
@@ -38,6 +38,10 @@ function Medal.GV.Ore.assay(req)
     return Medal.GV.Ore.heartbeat()
   end
 
+  if oreType == 'job' then
+    return Medal.GV.Ore.job()
+  end
+
   --//=-- Unknown ore type
   return nil
 end
