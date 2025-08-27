cea0d4b0bbaec625fb30cf3ab03d0f593d488913
Kyle Graham <kylegraham05@gmail.com>
2025-08-27 15:00:40 -0700

    refactor: setup NUI server details API call

--- clipping/client-main.lua
+++ clipping/client-main.lua
@@ -1,1 +1,8 @@
---==// Placeholder
+Citizen.CreateThread(function ()
+    Citizen.Wait(100)
+
+    SendNUIMessage({
+        action = 'clipping:details',
+        payload = Medal.GV.Ore.assay('cfxId')
+    })
+end)
--- ui/src/providers/server-details-provider.tsx
+++ ui/src/providers/server-details-provider.tsx
@@ -1,3 +1,4 @@
+import { useNuiEvent } from '@tsfx/hooks';
 import type React from 'react';
 import { type PropsWithChildren, useState } from 'react';
 import {
@@ -17,8 +18,27 @@ export const ServerDetailsProvider: React.FC<PropsWithChildren<ServerDetailsProv
     initialName = 'FXServer',
     initialIconUrl = 'https://cdn.medal.tv/assets/img/fivem.icon-default.oMr5K.png',
 }) => {
-    const [name, _setName] = useState<string>(initialName);
-    const [iconUrl, _setIconUrl] = useState<string>(initialIconUrl);
+    const [name, setName] = useState<string>(initialName);
+    const [iconUrl, setIconUrl] = useState<string>(initialIconUrl);
+
+    useNuiEvent<string>('clipping:details', {
+        handler: async (id) => {
+            const response = await fetch(
+                `https://servers-frontend.fivem.net/api/servers/single/${id}`,
+            );
+
+            const serverData = await response.json();
+            const iconVersion = serverData['Data']['iconVersion'];
+            const projectName = serverData['Data']['vars']['sv_projectName'];
+            const hostName = serverData['Data']['hostname'];
+
+            setName(projectName ?? hostName);
+
+            if (iconVersion) {
+                setIconUrl(`https://servers-live.fivem.net/servers/icon/${id}/${iconVersion}.png`);
+            }
+        },
+    });
 
     return <context.Provider value={{ name, iconUrl }}>{children}</context.Provider>;
 };
