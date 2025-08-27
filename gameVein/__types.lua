8c7a78c0890450bf6d2527ab847fd6e15f041ea2
Imthatguyhere <imthatguyhere@gmail.com>
2025-08-27 11:36:11 -0400

    feat(ore): add the job type for the new ore

--- gameVein/__types.lua
+++ gameVein/__types.lua
@@ -4,3 +4,9 @@
 
 ---@alias FrameworkKey 'qbx'|'qb'|'esx'|'ox'|'nd'|'tmc'|'unknown'
 ---@alias WsProtocol 'ws'|'wss'
+
+---@class Job
+---@field id string
+---@field name string
+---@field rank integer   --//=-- -1 indicates unknown
+---@field rankName string
