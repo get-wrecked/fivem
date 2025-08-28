cef921356ad86c2286c785f1667253c08c3d6552
Kyle Graham <kylegraham05@gmail.com>
2025-08-28 08:36:38 -0700

    feat: create clipping events config

--- config.lua
+++ config.lua
@@ -5,10 +5,25 @@ Config.Debug = true
 Config.Command = 'medal'
 Config.Keybind = 'pageup'
 
+Config.ClippingEvents = {
+    {
+        id = 'player_killed',
+        title = 'Player Killed',
+        desc = 'Triggers when you kill another player',
+        enabled = true
+    },
+    {
+        id = 'player_death',
+        title = 'Player Died',
+        desc = 'Triggers when you die',
+        enabled = true
+    }
+}
+
 Config.GameVein = {
-  WebSocket = {
-    host = "127.0.0.1",
-    port = 63325,
-    protocol = "ws",
-  }
+    WebSocket = {
+        host = "127.0.0.1",
+        port = 63325,
+        protocol = "ws",
+    }
 }
