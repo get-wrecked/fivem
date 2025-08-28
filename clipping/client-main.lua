b15b1831dbedf13f3bd26b844e7d56bdc45cc97a
Kyle Graham <kylegraham05@gmail.com>
2025-08-28 10:15:10 -0700

    feat(clipping): create read event config function

--- clipping/client-main.lua
+++ clipping/client-main.lua
@@ -3,6 +3,24 @@
 Medal = Medal or {}
 Medal.AC = Medal.AC or {} --//=-- The namespace for the client Auto Clipping functions
 
+function Medal.AC.readEventConfig(eventId)
+    local cfg = {}
+
+    if type(Config) == 'table' then
+        local events = Config.ClippingEvents
+
+        if type(events) == 'table' then
+            for _, event in ipairs(events) do
+                if event.id == eventId then
+                    cfg = event
+                end
+            end
+        end
+    end
+
+    return cfg
+end
+
 --//=-- Send the server Cfx Id to client NUI to retrieve server details, shortly after this resource starts
 AddEventHandler('onClientResourceStart', function (resourceName)
     if resourceName ~= GetCurrentResourceName() then return end
