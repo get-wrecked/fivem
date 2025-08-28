67a988501ce3ed72aacac10829bbd5720e08f51b
Kyle Graham <kylegraham05@gmail.com>
2025-08-28 07:49:28 -0700

    feat(lookout): create player kill handler

---
+++ clipping/lookout/client-player-kill.lua
@@ -0,0 +1,10 @@
+--//=-- Auto Clipping Lookout: Player Kill
+--//=-- Handles the Player Kill Signal
+
+Medal = Medal or {}
+Medal.AC = Medal.AC or {}
+Medal.AC.Lookout = Medal.AC.Lookout or {}
+
+function Medal.AC.Lookout.handlePlayerKill()
+    print('Local player killed another player')
+end
