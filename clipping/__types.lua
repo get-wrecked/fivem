84bb718da9d4e343eb4099cb7889546d2233ef93
Kyle Graham <kylegraham05@gmail.com>
2025-08-28 10:14:43 -0700

    chore(clipping): create type aliases

---
+++ clipping/__types.lua
@@ -0,0 +1,6 @@
+--//=-- Centralized type aliases for Auto Clipping
+
+---@meta
+
+---@alias EventConfig { id: string, title: string, desc?: string, enabled?: boolean }
+---@alias VesselCargo { eventId: string, eventName: string, tags?: string[] }
