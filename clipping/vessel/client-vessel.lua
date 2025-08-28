98a2f3e14d089d54121d3356342cd72336d8f1fd
Kyle Graham <kylegraham05@gmail.com>
2025-08-28 10:15:55 -0700

    feat(clipping): create client vessel

---
+++ clipping/vessel/client-vessel.lua
@@ -0,0 +1,9 @@
+--//=-- Auto Clipping Vessel: Sends normalized JSON payloads to NUI recorder connection
+
+Medal = Medal or {}
+Medal.AC = Medal.AC or {}
+
+---@param cargo VesselCargo
+function Medal.AC.vesselDepart(cargo)
+    -- TODO
+end
