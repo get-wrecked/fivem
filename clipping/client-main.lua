f5ee000479ac59398ec2153b1efbabded3e9ca70
Kyle Graham <kylegraham05@gmail.com>
2025-08-27 18:56:52 -0700

    refactor(clipping): setup AC namespace and startup event

--- clipping/client-main.lua
+++ clipping/client-main.lua
@@ -1,8 +1,18 @@
-Citizen.CreateThread(function ()
-    Citizen.Wait(100)
+-- Auto Clipping client
 
-    SendNUIMessage({
-        action = 'clipping:details',
-        payload = Medal.GV.Ore.assay('cfxId')
-    })
+Medal = Medal or {}
+Medal.AC = Medal.AC or {} --//=-- The namespace for the client Auto Clipping functions
+
+--//=-- Send the server Cfx Id to client NUI to retrieve server details, shortly after this resource starts
+AddEventHandler('onClientResourceStart', function (resourceName)
+    if resourceName ~= GetCurrentResourceName() then return end
+
+    CreateThread(function ()
+        Wait(100)
+
+        SendNUIMessage({
+            action = 'wt:details',
+            payload = Medal.GV.Ore.assay('cfxId')
+        })
+    end)
 end)
