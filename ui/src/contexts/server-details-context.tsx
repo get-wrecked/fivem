81c8d00405408ddda5dc3b348c1e5e68b1121cd1
Imthatguyhere <imthatguyhere@gmail.com>
2025-09-06 13:14:49 -0400

    docs: header for the ServerDetailsContext

--- ui/src/contexts/server-details-context.tsx
+++ ui/src/contexts/server-details-context.tsx
@@ -1,3 +1,19 @@
+/*
+  Medal.tv - FiveM Resource
+  =========================
+  File: ui/src/contexts/server-details-context.tsx
+  =====================
+  Description:
+    The server details context for the UI
+  ---
+  Exports & Exported Components:
+    - ServerDetailsContext : The server details context 
+    - ServerDetailsContextValue : The server details context's value interface
+  ---
+  Globals:
+    None
+*/
+
 import { createContext } from 'react';
 
 export interface ServerDetailsContextValue {
