310db451646eeb9e70afdb93edb4649e8cf2a176
Imthatguyhere <imthatguyhere@gmail.com>
2025-09-17 16:47:59 -0400

    feat: add API and WebSocket status providers to app providers stack

--- ui/src/providers.tsx
+++ ui/src/providers.tsx
@@ -16,8 +16,10 @@
 import { NuiProvider, NuiVisibilityProvider } from '@tsfx/hooks';
 import type { PropsWithChildren } from 'react';
 import NuiHandlers from './handlers/nui-handlers';
+import { ApiStatusProvider } from './providers/api-status-provider';
 import { ClipLengthProvider } from './providers/clip-length-provider';
 import { ServerDetailsProvider } from './providers/server-details-provider';
+import { WebSocketStatusProvider } from './providers/websocket-status-provider';
 
 /**
  * The React app's providers helper.
@@ -34,13 +36,17 @@ const Providers: React.FC<PropsWithChildren<{ debug?: boolean }>> = ({
     return (
         <NuiProvider debug={debug}>
             <NuiVisibilityProvider debug={debug}>
-                <ServerDetailsProvider>
-                    <ClipLengthProvider>
-                        {/*//=-- Registers listeners for messages from Lua/CFX */}
-                        <NuiHandlers />
-                        {children}
-                    </ClipLengthProvider>
-                </ServerDetailsProvider>
+                <ApiStatusProvider>
+                    <WebSocketStatusProvider>
+                        <ServerDetailsProvider>
+                            <ClipLengthProvider>
+                                {/*//=-- Registers listeners for messages from Lua/CFX */}
+                                <NuiHandlers />
+                                {children}
+                            </ClipLengthProvider>
+                        </ServerDetailsProvider>
+                    </WebSocketStatusProvider>
+                </ApiStatusProvider>
             </NuiVisibilityProvider>
         </NuiProvider>
     );
