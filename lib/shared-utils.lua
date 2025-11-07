ea22f16e56e57d3f8ced6797227385d68ca84c95
Imthatguyhere <imthatguyhere@gmail.com>
2025-11-07 10:14:21 -0500

    docs: update file header and add logBase64Payload documentation
    - Corrected file path reference in header comment from shared-common.lua to shared-utils.lua
    - Added header documentation for new logBase64Payload utility function

--- lib/shared-utils.lua
+++ lib/shared-utils.lua
@@ -1,7 +1,7 @@
 --[[
   Medal.tv - FiveM Resource
   =========================
-  File: lib/shared-common.lua
+  File: lib/shared-utils.lua
   =====================
   Description:
     Shared utility helpers
@@ -12,6 +12,7 @@
   Globals:
     - Medal.Shared.Utils : Shared utility helper functions
     - Medal.Shared.Utils.isValidCallback : Validates callback references, to make sure they are functions or function reference
+    - Medal.Shared.Utils.logBase64Payload : Logs Base64 payload lengths when Logger.debug is available
 ]]
 
 
@@ -32,4 +33,15 @@ function Medal.Shared.Utils.isValidCallback(cb)
     return true
   end
   return false
+end
+
+--- Log Base64 (data URI) payload length via Logger.debug when available
+---@param tag string
+---@param direction string
+---@param payload any
+function Medal.Shared.Utils.logBase64Payload(tag, direction, payload)
+  if type(payload) ~= 'string' then return end
+  if payload:sub(1, 5) ~= 'data:' then return end
+  if type(Logger) ~= 'table' or type(Logger.debug) ~= 'function' then return end
+  Logger.debug(tag, direction, 'Base64 Length:', #payload)
 end
