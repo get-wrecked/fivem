bb38bee05668e8ac987cecd494349041a5508042
Kyle Graham <kylegraham05@gmail.com>
2025-11-14 15:16:11 -0800

    chore(ui): add clip length translations

--- ui/src/components/clip-length.tsx
+++ ui/src/components/clip-length.tsx
@@ -15,11 +15,13 @@
 
 import { fetchNui } from '@tsfx/hooks';
 import type React from 'react';
+import { useTranslation } from 'react-i18next';
 import { useClipLength } from '@/hooks/use-clip-length';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
 
 export const ClipLength: React.FC = () => {
     const { length, setLength } = useClipLength();
+    const { t } = useTranslation();
 
     const updateLength = (value: string): void => {
         setLength(value);
@@ -28,19 +30,19 @@ export const ClipLength: React.FC = () => {
 
     return (
         <div className='w-full h-9 flex items-center justify-between font-normal'>
-            <span className='font-medium'>Clip Length</span>
+            <span className='font-medium'>{t('clip_length.title')}</span>
 
             <Select value={length} onValueChange={updateLength}>
                 <SelectTrigger className='w-32'>
                     <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
-                    <SelectItem value='15'>15 seconds</SelectItem>
-                    <SelectItem value='30'>30 seconds</SelectItem>
-                    <SelectItem value='1'>1 minute</SelectItem>
-                    <SelectItem value='2'>2 minutes</SelectItem>
-                    <SelectItem value='3'>3 minutes</SelectItem>
-                    <SelectItem value='5'>5 minutes</SelectItem>
+                    <SelectItem value='15'>{t('clip_length.seconds', { count: 15 })}</SelectItem>
+                    <SelectItem value='30'>{t('clip_length.seconds', { count: 30 })}</SelectItem>
+                    <SelectItem value='1'>{t('clip_length.minutes', { count: 1 })}</SelectItem>
+                    <SelectItem value='2'>{t('clip_length.minutes', { count: 2 })}</SelectItem>
+                    <SelectItem value='3'>{t('clip_length.minutes', { count: 3 })}</SelectItem>
+                    <SelectItem value='5'>{t('clip_length.minutes', { count: 5 })}</SelectItem>
                 </SelectContent>
             </Select>
         </div>
